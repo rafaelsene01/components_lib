429c4b5f45cb9a04f74438a6782af8bce1237b7e
rafaelsene01 <rafaelssene@gmail.com>
2021-01-27 10:50:39 -0300

    fix: css

--- src/components/TextField/text.vue
+++ src/components/TextField/text.vue
@@ -39,7 +39,7 @@ export default {
   }
 };
 </script>
-<style scoped>
+<style lang="postcss" scoped>
 .inputText {
   @apply block text-sm font-title-medium w-full px-4 py-2 mt-1 border border-paragraphy-4 rounded focus:border-primary-1 focus:ring-1 focus:ring-primary-1 focus:outline-none;
 }
